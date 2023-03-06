3dec7be50622daa2edf7dc76723e3d238a4fa1d7
me-cafebabe <me.cafebabe@gmail.com>
2023-03-06 17:49:49 +0800

    power:reset:msm-poweroff: Reboot to recovery when kernel panics

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -377,6 +377,14 @@ static void msm_restart_prepare(const char *cmd)
 	else
 		qpnp_pon_system_pwr_off(PON_POWER_OFF_HARD_RESET);
 
+	if (in_panic) {
+		// Reboot to recovery
+		qpnp_pon_set_restart_reason(
+			PON_RESTART_REASON_RECOVERY);
+		__raw_writel(0x77665502, restart_reason);
+		goto finish_set_restart_reason;
+	}
+
 #ifndef VENDOR_EDIT
 /* OPLUS 2013.07.09 hewei modify begin for restart mode*/
 	if (cmd != NULL) {
@@ -516,6 +524,7 @@ static void msm_restart_prepare(const char *cmd)
 /* OPLUS 2013.07.09 hewei modify en for restart mode*/
 #endif //VENDOR_EDIT
 
+finish_set_restart_reason:
 	flush_cache_all();
 
 	/*outer_flush_all is not supported by 64bit kernel*/
