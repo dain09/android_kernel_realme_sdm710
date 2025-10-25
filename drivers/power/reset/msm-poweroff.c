19e4a08a7f6f907287b2c0fbf65db9a3c9f67d3a
Yaroslav Zviezda <acroreiser@gmail.com>
2025-10-25 21:13:20 +0300

    msm_poweroff: force warm reboot
    Signed-off-by: Abdallah Ibrahim <iabdollah40@gmail.com>

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -350,22 +350,7 @@ static void msm_restart_prepare(const char *cmd)
 				(cmd != NULL && cmd[0] != '\0'));
 	}
 
-#ifdef OPLUS_BUG_STABILITY
-//Fanhong.Kong@PSW.BSP.CHG,add 2018/3/25 panic reboot reason as kernel for hotfix
-	if (in_panic){
-		//warm reset
-		qpnp_pon_system_pwr_off(PON_POWER_OFF_WARM_RESET);
-		qpnp_pon_set_restart_reason(
-					PON_RESTART_REASON_KERNEL);
-		flush_cache_all();
 
-		/*outer_flush_all is not supported by 64bit kernel*/
-#ifndef CONFIG_ARM64
-		outer_flush_all();
-#endif
-		return;
-	}
-#endif /* OPLUS_BUG_STABILITY */
 	force_warm_reboot = true;
 
 	if (force_warm_reboot)
